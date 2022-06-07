c0eda19fe1a388aa65b842bc4fc1d279f01cf18f
Ezequiel Silva <ezesil96@gmail.com>
2022-06-07 20:49:08 -0300

    Fix de mezclas con net6

--- Cinema.UI/Program.cs
+++ Cinema.UI/Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace Cinema.UI
 {
     internal static class Program
@@ -8,9 +14,9 @@ namespace Cinema.UI
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
